3f4df5201dc30dc27dd8699f5be84d41296f6320
Mihai Parvu <mparvu@localhost.localdomain>
2017-03-30 15:10:30 +0300

    added is_unicode import

--- src/SSHLibrary/pythonclient.py
+++ src/SSHLibrary/pythonclient.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from robot.utils import is_string, unic
+from robot.utils import is_string, unic, is_unicode
 
 import time
 import ntpath
